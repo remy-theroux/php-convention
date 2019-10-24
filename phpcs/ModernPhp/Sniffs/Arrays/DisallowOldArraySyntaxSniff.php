f880f3ec1b740601287bc088672905fb3723d3e7
Rémy Théroux <remy.theroux@gmail.com>
2019-10-24 15:04:44 +0200

    Add custom sniffs

---
+++ phpcs/ModernPhp/Sniffs/Arrays/DisallowOldArraySyntaxSniff.php
@@ -0,0 +1,33 @@
+<?php
+namespace ModernPhp\Sniffs\Arrays;
+
+use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Sniffs\Sniff;
+
+/**
+ * Class DisallowOldArraySyntax
+ */
+class DisallowOldArraySyntaxSniff implements Sniff
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function register()
+    {
+        return [
+            T_ARRAY,
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function process(File $phpcsFile, $stackPtr)
+    {
+        $phpcsFile->addError(
+            'Old array syntax (array()) is not allowed use [] instead',
+            $stackPtr,
+            'Invalid'
+        );
+    }
+}
